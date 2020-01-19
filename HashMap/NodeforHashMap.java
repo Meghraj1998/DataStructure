24a892e649c5c1f9487958894ef6eccb1037c844
Meghraj <meghraj.deshmukh103@gmail.com>
2020-01-19 14:46:52 +0530

    HashMap Implementation using BST

---
+++ HashMap/NodeforHashMap.java
@@ -0,0 +1,22 @@
+package HashMap;
+
+public class NodeforHashMap {
+	
+	int key;
+	int value;
+	NodeforHashMap right=null;
+	NodeforHashMap left=null;
+	
+	
+	NodeforHashMap (int k, int v)
+	{
+		
+		key=k;
+		value=v;
+		
+		
+	}
+	   
+	
+
+}
